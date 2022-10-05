34ec92614bea22f77f04a18f32a8ebceac3fa2ff
Delnat Wito <wito.delnat@gmail.com>
2022-10-05 14:40:35 +0200

    feat: improve table output

--- src/io.ts
+++ src/io.ts
@@ -1,26 +1,36 @@
-import { getRuleForResult, ValidationResponse } from "@monokle/validation";
+import {
+  getFileLocation,
+  getRuleForResult,
+  ValidationResponse,
+} from "@monokle/validation";
 import * as core from "@actions/core";
 import styles from "ansi-styles";
 
 export function printResponse(response: ValidationResponse) {
   for (const run of response.runs) {
-    core.info(`${styles.color.blue}Run ${run.tool.driver.name}`);
-
     if (run.results.length === 0) {
+      core.startGroup(
+        `${styles.color.blue.open}Run ${run.tool.driver.name}${styles.color.blue.close}`
+      );
       core.info("No problems detected");
+      core.endGroup();
       core.info("");
       continue;
     }
 
+    core.startGroup(
+      `${styles.color.red.open}Run ${run.tool.driver.name}${styles.color.red.close} (${run.results.length} problems)`
+    );
+
     for (const result of run.results) {
       const rule = getRuleForResult(response, result);
+      const location = getFileLocation(result);
 
-      core.startGroup(run.tool.driver.name);
       core.error(`[${result.ruleId}] ${result.message.text}`);
       core.error(
-        `  found at ${result.locations[0].physicalLocation?.artifactLocation.uri}`
+        `  found at ${location.physicalLocation?.artifactLocation.uri}:${location.physicalLocation?.region?.startColumn}`
       );
-      core.error(`    ${rule.fullDescription} ${rule.help}`);
+      core.error(`    ${rule.fullDescription.text} ${rule.help.text}`);
       core.endGroup();
     }
 
