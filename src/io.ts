f4908cacd2598a3f85b19c0e40fc09774b1a95db
f1ames <dr.odpowiedz@gmail.com>
2023-11-14 10:50:01 +0100

    fix: update core deps

--- src/io.ts
+++ src/io.ts
@@ -1,42 +1,65 @@
 import {
   getFileLocation,
-  getRuleForResult,
+  getRuleForResultV2,
   ValidationResponse,
+  ValidationResult,
+  ValidationRun,
 } from "@monokle/validation";
 import * as core from "@actions/core";
 import styles from "ansi-styles";
 
 export function printResponse(response: ValidationResponse) {
   for (const run of response.runs) {
-    if (run.results.length === 0) {
-      core.startGroup(
-        `${styles.color.green.open}Run ${run.tool.driver.name}${styles.color.green.close}`
-      );
-      core.info("No problems detected");
-      core.endGroup();
-      continue;
-    }
+    const groupedResults = groupResultsByExtension(run);
 
-    core.startGroup(
-      `${styles.color.red.open}Run ${run.tool.driver.name} (${run.results.length} problems)${styles.color.red.close}`
-    );
-
-    for (const result of run.results) {
-      const rule = getRuleForResult(response, result);
-      const location = getFileLocation(result);
-
-      const L1 = `[${result.ruleId}] ${result.message.text}`;
-      const L2 = `\n  found at ${location.physicalLocation?.artifactLocation.uri}:${location.physicalLocation?.region?.startColumn}`;
-      const L3 = `\n    ${rule.fullDescription?.text ?? rule.shortDescription.text}`;
-      const L4 = `\n      ${rule.help.text}`;
-
-      if (result.level === "error") {
-        core.error(`${L1}${L2}${L3}${L4}`);
-      } else {
-        core.warning(`${L1}${L2}${L3}${L4}`);
+    for (const [groupName, groupResults] of groupedResults) {
+      if (groupResults.length === 0) {
+        core.startGroup(
+          `${styles.color.green.open}Run ${groupName}${styles.color.green.close}`
+        );
+        core.info("No problems detected");
+        core.endGroup();
+        continue;
       }
-    }
 
-    core.endGroup();
+      core.startGroup(
+        `${styles.color.red.open}Run ${groupName} (${groupResults.length} problems)${styles.color.red.close}`
+      );
+
+      for (const result of groupResults) {
+        const rule = getRuleForResultV2(run, result);
+        const location = getFileLocation(result);
+
+        const L1 = `[${result.ruleId}] ${result.message.text}`;
+        const L2 = `\n  found at ${location.physicalLocation?.artifactLocation.uri}:${location.physicalLocation?.region?.startColumn}`;
+        const L3 = `\n    ${rule.fullDescription?.text ?? rule.shortDescription.text}`;
+        const L4 = `\n      ${rule.help.text}`;
+
+        if (result.level === "error") {
+          core.error(`${L1}${L2}${L3}${L4}`);
+        } else {
+          core.warning(`${L1}${L2}${L3}${L4}`);
+        }
+      }
+
+      core.endGroup();
+    }
   }
+}
+
+function groupResultsByExtension(responseRun: ValidationRun) {
+  const groups: Map<string, ValidationResult[]> = new Map(); // <extensionName, results>
+  const extensions = responseRun.tool.extensions ?? [];
+
+  for (const extension of extensions) {
+    groups.set(extension.name, []);
+  }
+
+  for (const result of responseRun.results) {
+    const extensionName = result.rule.toolComponent.name;
+
+    groups.get(extensionName)!.push(result);
+  }
+
+  return groups;
 }
--- src/readFiles.ts
+++ src/readFiles.ts
@@ -2,9 +2,9 @@ import { lstatSync } from "fs";
 import { readFile as readFileFromFs } from "fs/promises";
 import chunkArray from "lodash/chunk.js";
 import glob from "tiny-glob";
-import { File } from "./parse";
+import { BaseFile } from "@monokle/parser";
 
-export async function readFiles(path: string): Promise<File[]> {
+export async function readFiles(path: string): Promise<BaseFile[]> {
   if (isFileLike(path)) {
     const file = await readFile(path);
     return [file];
@@ -17,7 +17,7 @@ function isFileLike(path: string) {
   return lstatSync(path).isFile();
 }
 
-async function readFile(path: string): Promise<File> {
+async function readFile(path: string): Promise<BaseFile> {
   const content = await readFileFromFs(path, "utf8");
 
   return {
@@ -27,9 +27,9 @@ async function readFile(path: string): Promise<File> {
   };
 }
 
-async function readDirectory(directoryPath: string): Promise<File[]> {
+async function readDirectory(directoryPath: string): Promise<BaseFile[]> {
   const filePaths = await glob(`${directoryPath}/**/*.{yaml,yml}`);
-  const files: File[] = [];
+  const files: BaseFile[] = [];
 
   for (const chunk of chunkArray(filePaths, 5)) {
     const promise = await Promise.allSettled(
